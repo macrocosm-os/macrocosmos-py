eeccdae0299a284328e56a49d257bbd5b1175d39
cassova <29239592+cassova@users.noreply.github.com>
2025-07-01 17:09:56 +0100

    downgraded protobufs to support py3.8

--- src/macrocosmos/generated/apex/v1/apex_pb2_grpc.py
+++ src/macrocosmos/generated/apex/v1/apex_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 from macrocosmos.generated.apex.v1 import apex_pb2 as apex_dot_v1_dot_apex__pb2
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
-GRPC_GENERATED_VERSION = '1.73.0'
+GRPC_GENERATED_VERSION = '1.70.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
--- src/macrocosmos/generated/sn13/v1/sn13_validator_p2p.py
+++ src/macrocosmos/generated/sn13/v1/sn13_validator_p2p.py
@@ -1,6 +1,6 @@
 # This is an automatically generated file, please do not change
 # gen by protobuf_to_pydantic[v0.3.3.1](https://github.com/so1n/protobuf_to_pydantic)
-# Protobuf Version: 6.31.1 
+# Protobuf Version: 5.29.5 
 # Pydantic Version: 2.11.7 
 from google.protobuf.message import Message  # type: ignore
 from pydantic import BaseModel
